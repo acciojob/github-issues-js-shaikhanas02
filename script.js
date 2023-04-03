5fec160aa392bf444e532431e208fdf020dbc227
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-04-03 14:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 //your code here
-let i=1 ;
- async function fetchByPage (){
- const cont = document.getElementById
-('issue_container')
- const pi = document.getElementById
-('page_indicator')
- pi.innerText = `Page number ${i}`
- const rawData = await fetch(`https://api.
-github.com/repositories/1296269/issues?
-page=${i}&per_page=5`)
- const parsedData = await rawData.json()
- let store ='' ;
- parsedData.forEach(issue => {
- store += `<li>${issue.title}</li>`
- });
- cont.innerHTML = store ;
- }
- fetchByPage(i)
- function loadNext(){
- i++;
- fetchByPage() ;
- }
- function loadPrev(){
- if(i === 1) return;
- i--;
- fetchByPage() ;
- }
+
+let ol = document.getElementById('oll');
+
+fetch("https://api.github.com/repositories/1296269/issues?page=${1}&per_page=5.")
+    .then((response) => response.json())
+    .then((product) => {
+        document.getElementById('page_number').innerHTML = `Page Number 1`;
+        for (let i = 0; i < 5; i++) {
+            const element = product[i];
+            console.log(product[i].title);
+            ol.innerHTML += `<li>${product[i].title}</li>`
+        }
+    })
+
+let i = 1;
+
+let next = document.getElementById('load_next');
+let prev = document.getElementById('load_prev');
+
+next.addEventListener('click', () => {
+    i = i + 1;
+    fetch(`https://api.github.com/repositories/1296269/issues?page=${i}&per_page=5.`)
+        .then((response) => response.json())
+        .then((product) => {
+            document.getElementById('page_number').innerHTML = `Page Number ${i}`;
+            ol.innerHTML = '';
+            for (let z = 0; z < 5; z++) {
+                const element = product[z];
+                console.log(product[z].title);
+                ol.innerHTML += `<li>${product[z].title}</li>`
+            }
+        })
+})
+
+prev.addEventListener('click', () => {
+    if (i === 1) i = 1;
+    else i = i - 1;
+    fetch(`https://api.github.com/repositories/1296269/issues?page=${i}&per_page=5.`)
+        .then((response) => response.json())
+        .then((product) => {
+            document.getElementById('page_number').innerHTML = `Page Number ${i}`;
+            ol.innerHTML = '';
+            for (let z = 0; z < 5; z++) {
+                const element = product[z];
+                console.log(product[z].title);
+                ol.innerHTML += `<li>${product[z].title}</li>`
+                console.log(ol.innerHTML);
+            }
+        })
+})
